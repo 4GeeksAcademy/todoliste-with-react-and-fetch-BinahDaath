f140cb69056ffeb0b541cc51ca04c354243c4cb3
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-04 04:20:50 +0000

    done

--- src/js/component/todoliste.jsx
+++ src/js/component/todoliste.jsx
@@ -1,29 +1,38 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 export function TodoList() {
   const [todos, setTodos] = useState([
-    "Walk dog",
-    "Buy groceries",
-    "Study",
-    "Clean house",
   ]);
   const [task, setTask] = useState("");
-
+  const getTodos=async ()=>{
+    await fetch("https://playground.4geeks.com/todo/users/BinahDaath", {method: "POST",})
+    let a=(await (await fetch("https://playground.4geeks.com/todo/users/BinahDaath")).json()).todos;
+    a.reverse()
+    setTodos(a)
+  }
+  useEffect(()=>{getTodos()},[])
+  //useEffect(getTodos)
   const handleChangeTask = (ev) => {
     setTask(ev.target.value);
   };
 
-  const handleEnter = (ev) => {
+  const handleEnter = async (ev) => {
     if (ev.key === "Enter" && task.trim() !== "") {
-      setTodos([task.trim(), ...todos]);
-      setTask("");
+      
+      let t=task.trim()
       //fetch https://playground.4geeks.com/todo/todos/BinahDaath {"label":string,is_done:false}
+      let b={label:t,is_done:false};
+      let a=await (await fetch("https://playground.4geeks.com/todo/todos/BinahDaath",{method: "POST",headers:{'Content-type': 'application/json'},body:JSON.stringify(b)})).json();
+      console.log(a)
+      setTodos([a, ...todos]);
+      setTask("");
     }
   };
 
-  const handleDelete = (index) => {
+  const handleDelete = async (index,id) => {
     const newTodos = todos.filter((todo, i) => i !== index);
     setTodos(newTodos);
+    await fetch("https://playground.4geeks.com/todo/todos/"+id,{method: "DELETE"});
   };
 
   return (
@@ -40,10 +49,10 @@ export function TodoList() {
 
       <ul>
         {todos.map((todo, index) => (
-          <li key={index} className="flex items-center justify-between">
-            {todo}{" "}
+          <li key={todo.id} className="flex items-center justify-between">
+            {todo.label}{" "}
             <button
-              onClick={() => handleDelete(index)}
+              onClick={() => handleDelete(index,todo.id)}
               className="px-1 bg-red-400 hover:bg-red-700 text-white text-xs rounded-lg"
             >
               x
